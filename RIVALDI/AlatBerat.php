f581d6b287a384fd168c77019422f61a4554cb75
pakdekita <71884013+pakdekita@users.noreply.github.com>
2020-10-25 18:47:38 +0700

    Add files via upload

---
+++ RIVALDI/AlatBerat.php
@@ -0,0 +1,64 @@
+<!-- Navbar -->
+
+
+<link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/pricing/">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- Bootstrap core CSS -->
+<link href="/docs/4.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <div class="topnav">
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+            <a class="navbar-brand" href="#">CENTRAL</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="Form Rental.php">Berlian</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Maria</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Richard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Rivaldi</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <pre></pre>
+    <pre></pre>
+  <div class="container">
+  <div class="card-deck mb-3 text-left">
+    <div class="card mb-4 shadow-sm">
+      <div class="card-body">
+        <label for="name">Heavy Equipment</label>
+        <input type="text" class="form-control" id="name" name="name">
+        <label for="Type">Type</label>
+        <input type="text" class="form-control" id="Type" name="Type">
+        <label for="Cap">Capacity</label>
+        <input type="text" class="form-control" id="Cap" name="Cap">
+        <label for="AreaBoss">Specification</label>
+        <textarea class="form-control" id="Speks" rows="3" placeholder="Example: 1 Unit Volvo SD100D C"></textarea>
+        <label for="Price1">Price</label>
+        <input type="text" class="form-control" id="Price1" name="Price1">
+        <small class="form-text text-muted">Rupiah(Rp)</small>
+        <label for="Ava">Availability</label>
+        <input type="text" class="form-control" id="Ava" name="Ava">
+        <label for="Loc">Location</label>
+        <input type="Loc" class="form-control" id="Loc" name="Loc">
+        
+        <pre></pre>
+        <form method="POST" action="#">
+            <button class="btn btn-primary btn-lg active" type="submit" name="add" value="add">Add</button>
+        </form>
+      </div>
+    </div>
+    
+  </div>
+</div>
+</body>
+</html>  
